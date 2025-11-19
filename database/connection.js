2b9dd087955ee7979f4294adc2bc5fac18383872
Elilaw179 <sundayelisha179@gmail.com>
2025-11-19 13:05:39 +0100

    Fixing errors

--- database/connection.js
+++ database/connection.js
@@ -1,27 +1,14 @@
 
 
 
-// import pg from "pg";
-
-// const pool = new pg.Pool({
-//   host: "localhost",
-//   port: 5432,
-//   database: "RenderDB",
-//   user: "postgres",
-//   password: "12345"
-// });
-
-// export default pool;
-
-
-
 import pg from "pg";
 
 const pool = new pg.Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
+  host: "localhost",
+  port: 5432,
+  database: "RenderDB",
+  user: "postgres",
+  password: "12345"
 });
 
 export default pool;
