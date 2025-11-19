a6bf869ef49edfb595d8c3c36725f2e529355ebd
Elilaw179 <sundayelisha179@gmail.com>
2025-11-19 12:57:19 +0100

    debugging

--- database/connection.js
+++ database/connection.js
@@ -1,14 +1,27 @@
 
 
 
+// import pg from "pg";
+
+// const pool = new pg.Pool({
+//   host: "localhost",
+//   port: 5432,
+//   database: "RenderDB",
+//   user: "postgres",
+//   password: "12345"
+// });
+
+// export default pool;
+
+
+
 import pg from "pg";
 
 const pool = new pg.Pool({
-  host: "localhost",
-  port: 5432,
-  database: "RenderDB",
-  user: "postgres",
-  password: "12345"
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 export default pool;
